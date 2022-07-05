9fe2599d57e0aeb8724b2cb765cb47e92571f738
timstudhshl <92087127+timstudhshl@users.noreply.github.com>
2022-07-05 22:11:13 +0200

    TicTacToe fertig
    Usereingabe nach Zeile und Spalte
    Wenn jemand gewonnen hat oder kein Feld mehr frei ist bricht das Programm ab

--- src/ErklaerBaer/Program.cs
+++ src/ErklaerBaer/Program.cs
@@ -5,18 +5,56 @@ namespace ErklaerBaer
 {
     internal static class Program
     {
+        private static char _aktuellerSpieler = 'X';
         public static void Main()
         {
             var feld = new Feld();
-            feld.Ausgeben();
-            feld.SpielsteinSetzen(1, 0, 'X');
-            feld.Ausgeben();
-            feld.SpielsteinSetzen(1, 2, 'O');
-            feld.Ausgeben();
+
+            var richtigeEingabe = false;
+            do
+            {
+                Console.Clear();
+                if (richtigeEingabe) // Wenn der Spieler eine richtige Eingabe getätigt hat kann der Spieler gewechselt werden
+                    SpielerWechseln();
+
+                feld.Ausgeben();
+
+                Console.Write($"{_aktuellerSpieler} bitte geben Sie zuerst die ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Zeile");
+                Console.ResetColor();
+                Console.Write(" ein: ");
+                var zeile = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Geben Sie nun die ");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write("Spalte");
+                Console.ResetColor();
+                Console.Write(" ein: ");
+                var spalte = Convert.ToInt32(Console.ReadLine());
+
+                richtigeEingabe =
+                    feld.SpielsteinSetzen(zeile, spalte, _aktuellerSpieler);
+
+            } while (feld.WeitereZuegeMoeglich()); // Solange die Eingabe nicht korrekt ist frage den Nutzer nach einer Zeile und Spalte
+
+            // \n bedeutet das man eine neue Zeile anfangen möchte innerhalb eines strings
+            // man spart sich also ein neuen Console.WriteLine aufruf
+            Console.WriteLine("\nDas Spiel ist vorbei jemand hat gewonnen oder es sind keine Felder mehr frei!");
 
             // Nur das man noch eine Taste nach durchlauf des Programms drücken muss, bis dies sich beendet.
             // Consolenausgabe lesen können
             Console.ReadLine();
+        }
+
+        private static void SpielerWechseln()
+        {
+            if (_aktuellerSpieler == 'X')
+                _aktuellerSpieler = 'O';
+            else
+                _aktuellerSpieler = 'X';
+
+            // _aktuellerSpieler = _aktuellerSpieler == 'X' ? 'O' : 'X'; kürzere Version der If Abfrage
         }
     }
 }
--- src/ErklaerBaer/TicTacToe/Feld.cs
+++ src/ErklaerBaer/TicTacToe/Feld.cs
@@ -20,10 +20,59 @@ namespace ErklaerBaer.TicTacToe
 
         public bool SpielsteinSetzen(int zeile, int spalte, char zeichen)
         {
+            if (zeile > 2 || spalte > 2) return false;
             if (_feld[zeile, spalte] != ' ') return false;
 
             _feld[zeile, spalte] = zeichen;
             return true;
+        }
+
+        public bool SpielerGewonnen(char zeichen)
+        {
+            for (var index = 0; index < 3; index++)
+            {
+                // Vertikal
+                if (_feld[0, index] == zeichen &&
+                    _feld[1, index] == zeichen &&
+                    _feld[2, index] == zeichen)
+                    return true;
+
+                // Horizontal
+                if (_feld[index, 0] == zeichen &&
+                    _feld[index, 1] == zeichen &&
+                    _feld[index, 2] == zeichen)
+                    return true;
+            }
+
+            // Diagonal: oben Links => unten Rechts
+            if (_feld[0, 0] == zeichen &&
+                _feld[1, 1] == zeichen &&
+                _feld[2, 2] == zeichen)
+                return true;
+
+            // Diagonal: unten Links => oben Rechts
+            if (_feld[0, 2] == zeichen &&
+                _feld[1, 1] == zeichen &&
+                _feld[2, 0] == zeichen)
+                return true;
+
+            return false;
+        }
+
+        public bool WeitereZuegeMoeglich()
+        {
+            if (SpielerGewonnen('X') || SpielerGewonnen('O'))
+                return false; // Wenn irgend ein Spieler schon gewonnen hat sind keine Züge mehr möglich
+
+            for (var zeile = 0; zeile < 3; zeile++)
+            {
+                for (var spalte = 0; spalte < 3; spalte++)
+                {
+                    if (_feld[zeile, spalte] == ' ') return true; // Wenn noch keiner Gewonnen hat und noch ein Feld frei ist, ist auch noch ein Zug möglich
+                }
+            }
+
+            return false; // Wenn keiner Gewonnen hat aber alle Felder belegt sind ist es ein Unentschieden und keine Züge sind mehr möglich
         }
 
         public void Ausgeben()
