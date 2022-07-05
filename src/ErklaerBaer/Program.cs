9971519d68fe2d5d3e749e98da5e3893f7ab3dfa
timstudhshl <92087127+timstudhshl@users.noreply.github.com>
2022-07-05 21:40:42 +0200

    TicTacToe basics
    1. Feld erstellen
    2. Spielstein setzen

---
+++ src/ErklaerBaer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ErklaerBaer.TicTacToe;
+
+namespace ErklaerBaer
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            var feld = new Feld();
+            feld.Ausgeben();
+            feld.SpielsteinSetzen(1, 0, 'X');
+            feld.Ausgeben();
+            feld.SpielsteinSetzen(1, 2, 'O');
+            feld.Ausgeben();
+
+            // Nur das man noch eine Taste nach durchlauf des Programms drücken muss, bis dies sich beendet.
+            // Consolenausgabe lesen können
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/ErklaerBaer/TicTacToe/Feld.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ErklaerBaer.TicTacToe
+{
+    internal class Feld
+    {
+        private readonly char[,] _feld = new char[3, 3]; // readonly kann man auch weg lassen, ist nur dafür da das man das Feld nicht überschreiben kann
+
+        public Feld()
+        {
+            // Beim erstellen eines Feldes einmal alle Felder mit einem leeren Char (Leertaste) füllen
+            for (var zeile = 0; zeile < 3; zeile++)
+            {
+                for (var spalte = 0; spalte < 3; spalte++)
+                {
+                    _feld[zeile, spalte] = ' ';
+                }
+            }
+        }
+
+        public bool SpielsteinSetzen(int zeile, int spalte, char zeichen)
+        {
+            if (_feld[zeile, spalte] != ' ') return false;
+
+            _feld[zeile, spalte] = zeichen;
+            return true;
+        }
+
+        public void Ausgeben()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("   0  1  2");
+            for (var zeile = 0; zeile < 3; zeile++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"{zeile} ");
+                Console.ResetColor();
+                for (var spalte = 0; spalte < 3; spalte++)
+                {
+                    // Ausgabe des Wertes im Array an der Stelle (Zeile, Spalte)
+                    Console.Write($"[{_feld[zeile, spalte]}]"); // Console.Write("[" + _feld[zeile, spalte] + "]"); Macht das gleiche
+                }
+                Console.WriteLine(); // Absatz nach jeder Zeile
+            }
+        }
+    }
+}
